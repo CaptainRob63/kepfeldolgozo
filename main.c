e0379dff45802c9c7ea8336f1cae4b5138de8c7f
banana63 <fotyek.robert63@gmail.com>
2024-11-24 13:37:52 +0100

    process felkesz

--- main.c
+++ main.c
@@ -8,7 +8,55 @@
 #include "util.h"
 #include "iras.h"
 #include "debugmalloc.h"
+float matrix_normalisation_constant(Matrix matrix) {
+    float acc = 0;
+    for (int i = 0; i < matrix.array.height; ++i) {
+        for (int j = 0; j < matrix.array.width; ++j) {
+            acc += matrix.array.data[i][j];
+        }
+    }
+    return acc;
+}
 
+void process_pixel(Image *img, int x, int y, Matrix matrix, float norm) {
+    float accR = 0;
+    float accG = 0;
+    float accB = 0;
+
+    int startPosX = x - (matrix.array.width  - 1) / 2;
+    int startPosY = y - (matrix.array.height - 1) / 2;
+
+    for (int i = 0; i < matrix.array.height; ++i) {
+        for (int j = 0; j < matrix.array.width; ++j) {
+
+            if (startPosX + i  <  0           |
+                startPosX + i  >  img->width  |
+                startPosY + j  <  0           |
+                startPosY + j  >  img->height )
+                continue;
+
+            accR += matrix.array.data[i][j] * img->array1 [startPosX + i]  [startPosY + j].red;
+            accG += matrix.array.data[i][j] * img->array1 [startPosX + i]  [startPosY + j].green;
+            accB += matrix.array.data[i][j] * img->array1 [startPosX + i]  [startPosY + j].blue;
+        }
+    }
+
+    img->array1[x][y].red   = accR / norm;
+    img->array1[x][y].green = accG / norm;
+    img->array1[x][y].blue  = accB / norm;
+    //printf("%d %d\n",x,y);
+}
+
+void process_image(Image *img, Matrix matrix) {
+    float norm = matrix_normalisation_constant(matrix);
+
+    for (int i = 0; i < img->height; ++i)
+        for (int j = 0; j < img->width; ++j)
+            process_pixel(img, j, i, matrix, norm);
+
+
+
+}
 
 
 int main(int argc, char *argv[]) {
@@ -26,11 +74,17 @@ int main(int argc, char *argv[]) {
     img = read_image(fp);
     fclose(fp);
 
+    Matrix matrix = read_matrix("asdasdasd", "5", "5", "0 0 1 0 0 0 1 1 1 0 1 1 1 1 1 0 1 1 1 0 0 0 1 0 0 ");
+
     fp = fopen("img.ppm", "wb");
+
+
+    process_image(&img, matrix);
 
     write_image_P6(&img, fp);
 
-    image_free(&img);
+    matrix_free(&matrix);
+    //image_free(&img);
 /*
     Matrix matrix = read_matrix("asdasdasd", "4", "3", "1 2 3 4 5 6 7 8 9 10 11 12");
     //Matrix matrix = read_matrix(argv[1], argv[2], argv[3], argv[4]);
